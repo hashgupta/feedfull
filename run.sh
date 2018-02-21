9bcf792b2afd8f34e13f8c593ff378f974e2cbc6
Yash <yash.gupta.7782@gmail.com>
2018-02-21 09:32:18 -0500

    fixed

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y upgrade
+# sudo apt-get update
+# sudo apt-get -y upgrade
 
-sudo curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
+# sudo curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
 
-sudo tar -xvf go1.9.1.linux-amd64.tar.gz
+# sudo tar -xvf go1.9.1.linux-amd64.tar.gz
 
-sudo mv go /usr/local
+# sudo mv go /usr/local
 
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+# echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 
 go get github.com/pebbe/zmq4
 
