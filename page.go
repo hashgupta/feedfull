8bcbff5f8c33c996b2c16101a79d325fa16f268c
Yash <yash.gupta.7782@gmail.com>
2018-02-26 21:14:15 -0500

    almost

--- page.go
+++ page.go
@@ -89,8 +89,10 @@ func main() {
 
 	start := flow.New("indexing").Channel(data)
 
+	start.Printlnf("%s - %s - %d - %s")
+
 	keywords := start.Select("divert_keywords", flow.Field(1,4))
-	keywords.Printlnf("%s - %s")
+	// keywords.Printlnf("%s - %s")
 	// word_doc_one := start.Select(1,4).
 	// 	PartitionByKey("partition", 7).
 	// 	Map("read content", registeredReadConent)
